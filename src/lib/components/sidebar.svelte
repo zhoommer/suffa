3e5163006a7c6aaf065dece96012f5a6e7d558c5
devaslife <akiff.fazill@gmail.com>
2026-07-04 16:46:48 +0300

    feat: add sidebar ui

---
+++ src/lib/components/sidebar.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+	import { House, Library, User, ScrollText, Signal } from '@lucide/svelte';
+</script>
+
+<aside>
+	<div class="container">
+		<ul class="list">
+			<li class="list-item">
+				<a href="/" class="list-item-link">
+					<House size={20} />
+					<span>Anasayfa</span>
+				</a>
+			</li>
+			<li class="list-item">
+				<a href="/library" class="list-item-link">
+					<Library size={20} />
+					<span>Kütüphane</span>
+				</a>
+			</li>
+			<li class="list-item">
+				<a href="/profile" class="list-item-link">
+					<User size={20} />
+					<span>Profil</span>
+				</a>
+			</li>
+			<li class="list-item">
+				<a href="/hitories" class="list-item-link">
+					<ScrollText size={20} />
+					<span>Hikayeler</span>
+				</a>
+			</li>
+			<li class="list-item">
+				<a href="/stats" class="list-item-link">
+					<Signal size={20} />
+					<span>İstatistikler</span>
+				</a>
+			</li>
+		</ul>
+	</div>
+</aside>
+
+<style>
+	aside {
+		width: 250px;
+		height: 100vh;
+		background-color: var(--color-primary-50);
+	}
+
+	.list {
+		border-bottom: 1px solid var(--color-border-divider);
+		padding-bottom: 2rem;
+	}
+
+	.list-item {
+		position: relative;
+		list-style: none;
+		padding: 1rem;
+		transition: font-weight 0.3s ease;
+	}
+
+	.list-item.active::before {
+		content: '';
+		position: absolute;
+		left: 2px;
+		top: 8px;
+		width: 4px;
+		height: 70%;
+		background-color: var(--color-primary-950);
+		transition: background-color 0.3s ease;
+	}
+
+	.list-item:hover {
+		font-weight: 500;
+	}
+
+	.list-item-link {
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+		gap: 0.8rem;
+		text-decoration: none;
+		color: var(--color-primary-950);
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import Aside from '$lib/components/sidebar.svelte';
+	let { children } = $props();
+</script>
+
+<Aside />
+{@render children()}
+
+<style>
+	:global(:root) {
+		/* Background Colors */
+		--color-bg-body: #fafaf9;
+		--color-bg-surface: #ffffff;
+
+		/* Border & Divider Colors */
+		--color-border-divider: #e7e5e4;
+
+		/* Typography Colors */
+		--color-text-heading: #0f172a;
+		--color-text-body: #334155;
+		--color-text-muted: #64748b;
+
+		/* Primary Brand Colors (Deep Pine Green) */
+		--color-primary-50: #ecfdf5;
+		--color-primary-100: #d1fae5;
+		--color-primary-400: #34d399;
+		--color-primary-500: #10b981;
+		--color-primary-600: #059669; /* Main Action Color */
+		--color-primary-700: #047857; /* Hover State */
+		--color-primary-800: #065f46;
+		--color-primary-950: #022c22;
+	}
+
+	:global(*) {
+		margin: 0;
+		padding: 0;
+	}
+
+	:global(body) {
+		font-family: system-ui, sans-serif;
+		box-sizing: border-box;
+		background-color: var(--color-bg-body);
+		font-style:
+			Times New Roman,
+			sans-serif;
+	}
+</style>

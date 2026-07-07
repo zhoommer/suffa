3234013a14b92cd2e0d2e3dd5628010111ee60ce
devaslife <akiff.fazill@gmail.com>
2026-07-07 15:51:25 +0300

    fix(route): fix api routes

--- src/routes/api/auth/[...auth]/+server.ts
+++ src/routes/api/auth/[...auth]/+server.ts
@@ -1,3 +1,13 @@
+import type { RequestHandler } from '@sveltejs/kit';
 import { auth } from '$lib/server/auth';
 
-export const { GET, POST } = auth.handler;
+export const GET: RequestHandler = async (event) => {
+	return auth.handler(event.request);
+};
+
+export const POST: RequestHandler = async (event) => {
+	return auth.handler(event.request);
+};
+
+
+
